8f42cad87a2db1bea76072557d5a58d4d8070d8f
saposki <saposki@gmail.com>
2016-06-09 03:44:32 -0700

    completed "checkNum.js"

---
+++ checkNums.js
@@ -0,0 +1,9 @@
+ //Return the string "true" if the second integer
+// parameter (num2) is larger than the first (num1).
+
+
+function check(num1, num2){
+   return (num2 > num1);
+}
+
+console.log(check(2,5));
