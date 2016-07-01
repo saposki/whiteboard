0001119376eb82ce7c60c6ce871b5386fe83e06c
saposki <saposki@gmail.com>
2016-06-30 19:06:57 -0700

    result format

--- bsa.js
+++ bsa.js
@@ -18,23 +18,28 @@ var binarySearch = function (array, target) {
 
  var min = 0;
  var max = array.length -1;
+ var result = [];
+
  guess = guesser(max, min);
 
  while(min <= max){
      if(target === array[guess]){
-       console.log('found at index', array.indexOf(array[guess]));
+        result.push(array.indexOf(array[guess]));
+        console.log(result);
        return;
 
      }else if(array[guess] < target){
        guess += 1;
        if(target === array[guess]){
-          console.log('found at index', array.indexOf(array[guess]));
+          result.push(array.indexOf(array[guess]));
+          console.log(result);
           return;
        }
      }else if(array[guess] > target){
        guess -= 1;
        if(target === array[guess]){
-          console.log('found at index', array.indexOf(array[guess]));
+          result.push(array.indexOf(array[guess]));
+          console.log(result);
           return;
        }
      }
@@ -43,4 +48,4 @@ var binarySearch = function (array, target) {
 
 };
 
-binarySearch([1,2,3,4,5,6,7,8,9,10,11,12], 12);
+binarySearch([1,2,3,4,5,6,7,8,9,10,11,12], 1);
