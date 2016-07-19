a838243a3ec14e4c9222ac943028c32a4c512798
saposki <saposki@gmail.com>
2016-07-18 18:25:55 -0700

    added allUniqueChar.js

---
+++ allUniqChar.js
@@ -0,0 +1,10 @@
+// Implement an algorithm to determine if a string has all unique characters
+
+var unique = function(string){
+      var obj = {};
+      var count = 0;
+      for(var i = 0; i < string.length; i++){
+      
+      }
+      return count;
+}
