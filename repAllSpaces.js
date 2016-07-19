d4237285305ab55932655e8bcbdef4b05ae1e453
saposki <saposki@gmail.com>
2016-07-18 18:05:01 -0700

    completed repAllSpaces.js

--- repAllSpaces.js
+++ repAllSpaces.js
@@ -1,3 +1,19 @@
 // Write a method to replae all spaces in a string with "%20".
 // Input: "Mr John Smith ", 13
 // Output: "Mr%20John%20Smith"
+
+var constString  = "%20"
+var rep = function(string){
+   var returnString = "";
+   for(var i = 0; i < string.length; i++){
+      if(string.charAt(i)=== ' ')
+      {
+         returnString += constString;
+         i++;
+      }
+      returnString += string.charAt(i)
+   }
+   return returnString;
+}
+
+console.log(rep('Mr John Smith '));
