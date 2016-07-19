1cb59b2dc3aa96e73a6f3017b737dadb35f73501
saposki <saposki@gmail.com>
2016-07-18 18:26:36 -0700

    modified

--- allUniqChar.js
+++ allUniqChar.js
@@ -4,7 +4,15 @@ var unique = function(string){
       var obj = {};
       var count = 0;
       for(var i = 0; i < string.length; i++){
-      
+         if(string.charAt(i) in  obj){
+            obj[string.charAt(i)] = obj[string.charAt(i)] + 1;
+            count++;
+         }else{
+            obj[string.charAt(i)] = 1;
+         }
       }
       return count;
 }
+
+
+console.log(unique('acbc'));
